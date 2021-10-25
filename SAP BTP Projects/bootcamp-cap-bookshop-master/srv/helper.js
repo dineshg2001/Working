8ccc41d93f63c44ba3716264792d9cf84294b076
Dinesh Ghanta <dinesh.ghanta@ey.com>
2021-10-25 14:56:11 +0000

    Added bookshop

---
+++ SAP BTP Projects/bootcamp-cap-bookshop-master/srv/helper.js
@@ -0,0 +1,54 @@
+const { BusinessPartner } = require("@sap/cloud-sdk-vdm-business-partner-service");
+const _prepareBpBody = (bp) => {
+    return {
+        firstName: bp.FirstName,
+        lastName: bp.LastName,
+        businessPartnerCategory: bp.BusinessPartnerCategory
+    }
+}
+const buildBusinessPartnerForCreate = (data) => {
+    const bp = BusinessPartner.builder().fromJson(_prepareBpBody(data));
+    return bp;
+}
+
+function formatBPResultsForCAPOData(businessPartners) {
+    const jsonFormatForCAPOdataBP = [];
+    businessPartners.forEach(businessPartner => {
+        jsonFormatForCAPOdataBP.push(
+            {
+                BusinessPartner: businessPartner.businessPartner,
+                FirstName: businessPartner.firstName,
+                LastName: businessPartner.lastName,
+                Industry: businessPartner.Industry,
+                BusinessPartnerCategory: businessPartner.businessPartnerCategory,
+                FullName: businessPartner.businessPartnerFullName,
+                PersonNumber: businessPartner.personNumber
+            });
+    });
+    return jsonFormatForCAPOdataBP;
+}
+
+function cleanJsonDuplicates(arr) {
+    var cleaned = new Map();
+    arr.forEach(function (item) {
+        cleaned.set(JSON.stringify(item), item);
+    });
+
+    return [...cleaned.values()];
+}
+
+function convert2CDSFormat(businessPartner) {
+    return {
+        BusinessPartner: businessPartner.businessPartner,
+        FirstName: businessPartner.firstName,
+        LastName: businessPartner.lastName,
+        BusinessPartnerCategory: businessPartner.businessPartnerCategory
+    };
+}
+
+module.exports = {
+    buildBusinessPartnerForCreate,
+    formatBPResultsForCAPOData,
+    cleanJsonDuplicates,
+    convert2CDSFormat
+}
